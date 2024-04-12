de547967e7a2b095d91ad6aa0c555a9a03ab3d2e
JoseJoaquinMartinez <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-04-12 19:26:19 +0000

    feat: first model scheme and get people/vehicle/planet endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Vehicles, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -45,6 +45,40 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/people')
+def get_people():
+    people = People.query.all()
+    return jsonify([p.serialize for p in people])
+
+@app.route('/people/<int:people_id>')
+def get_people_by_id(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify(person.serialize)
+
+@app.route('/planets')
+def get_people():
+    planets = Planet.query.all()
+    return jsonify([p.serialize for p in planets])
+
+@app.route('/planets/<int:planet_id>')
+def get_planet_by_id(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify(planet.serialize)
+
+@app.route('/vehicles')
+def get_vehicles():
+    vehicles = Vehicles.query.all()
+    return jsonify(v.serialize for v in vehicles)
+
+@app.route('/vehicles/<int:vehicle_id>')
+def get_vehicle_by_id(vehicle_id):
+    vehicle = Vehicles.query.get_or_404(vehicle_id)
+    return jsonify(vehicle.serialize)
+
+
+
+
+        
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = relationship('Favorite', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +21,47 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class People (db.Model):
+    people_id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(50), nullable=False, unique=True)
+    height =db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(15))
+    skin_color=db.Column(db.String(15))
+    eye_color=db.Column(db.String(15))
+    birth_year=db.Column(db.String(20))
+    gender=db.Column(db.String(20))
+    homeworld=db.Column(db.String(30))
+
+class Planet (db.Model):
+    planet_id= db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(50), unique=True, nullable=False)
+    diameter=db.Column(db.Integer)
+    rotation_period=db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity= db.Column(db.Integer)
+    population =db.Column(db.Integer)
+    climate= db.Column(db.String(50))
+    terrain= db.Column (db.String(50))
+    surface_water = db.Column(db.Integer)
+
+class Vehicles (db.Model):
+    vehicle_id= db.Column(db.Integer, primary_key=True)
+    vehicle_name = db.Column(db.String(80), unique=True, nullable=False)
+    cargo_capacity= db.Column(db.Integer)
+    cost_in_credits= db.Column(db.Integer)
+    created= db.Column(db.String(80))
+    crew = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    manufacturer=db.Column(db.String(80))
+    model = db.Column(db.String(80))
+    passengers= db.Column(db.Integer)
+    vehicle_class= db.Column(db.String(50))
+    
+class Favorite (db.Model):
+    favourite_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey(User.user_id))
+    character_id = db.Column(db.Integer, ForeignKey(People.people_id))
+    planet_id = db.Column(db.Integer, ForeignKey(Planet.planet_id))
+    vehicle_id = db.Column(db.Integer, ForeignKey(Vehicles.vehicle_id))
+    favourite_type = db.Column (db.String(10), nullable=False)
